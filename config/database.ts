9c4d28f3313379bfe7f72bf39a6e1422bf90e7c1
lauti <lautaro.arnay@mangoodev.com>
2022-10-30 17:50:03 -0300

    commit

--- config/database.ts
+++ config/database.ts
@@ -1,13 +1,35 @@
-export default ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const parse = require('pg-connection-striung').parse;
+
+export default ({ env }) => {{
+
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: false,
+        },
+      },
+    }
+  }  
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'agustinesDB'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '12345'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'agustinesDB'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', '12345'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
-  },
-});
+  }
+}};
